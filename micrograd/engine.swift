cd194cf17e9d0436252e9d6a1690adf08520b0f2
Kruthay Kumar Reddy Donapati <kruthay@gmail.com>
2024-01-24 17:46:13 -0500

    Updated Value class to conform to Numeric Protocol

--- micrograd/engine.swift
+++ micrograd/engine.swift
@@ -8,31 +8,42 @@ import Foundation
 
 infix operator ** :  MultiplicationPrecedence
 
-class Value: CustomStringConvertible, Equatable, Hashable {
-    
-    var description: String {
-        return "\(label) d: \(String(format: "%.4f",data)), g: \(String(format: "%.4f",grad)) "
-    }
+final class Value {
     
     var data : Double
-    var children: [Value]
-    var operations: String
-    var grad: Double
+    var children: [Value] = []
+    var operations: String = ""
+    var grad: Double = 0.0
     var label: String = ""
-    
-    var _backward: () -> Void
+    var _backward: () -> Void = {}
     
     init(_ data: Double, children: [Value] = [], operations: String = "") {
         self.data = data
         self.children = children
         self.operations = operations
-        self.grad = 0.00
-        self._backward = {}
     }
+}
+
+extension Value: CustomStringConvertible {
+    var description: String {
+        return "\(label) d: \(String(format: "%.4f",data)), g: \(String(format: "%.4f",grad)) "
+    }
+}
+
+extension Value: Hashable {
     
     static func == (lhs: Value, rhs: Value) -> Bool {
-            return lhs.data == rhs.data && lhs.children == rhs.children && lhs.operations == rhs.operations && lhs.grad == rhs.grad
-        }
+        return lhs.data == rhs.data && lhs.grad == rhs.grad && lhs.children == rhs.children
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.data)
+        hasher.combine(self.grad)
+    }
+    
+}
+
+extension Value: AdditiveArithmetic {
     
     static func + (lhs: Value, rhs: Value) -> Value {
         let out = Value(lhs.data + rhs.data, children: [lhs, rhs], operations: "+")
@@ -42,7 +53,6 @@ class Value: CustomStringConvertible, Equatable, Hashable {
 
         }
         return out
-        
     }
     
     static func + (lhs: Value, rhs: Double) -> Value {
@@ -54,7 +64,26 @@ class Value: CustomStringConvertible, Equatable, Hashable {
         return Value(lhs) + rhs
     }
     
-    static func - (lhs: Value, rhs: Value) -> Value {
+}
+
+extension Value : Numeric {
+        var magnitude: Double {
+        return Swift.abs(data)
+    }
+    
+    convenience init(integerLiteral value: Int) {
+        self.init(Double(value))
+    }
+    
+    typealias Magnitude = Double
+    
+    convenience init?<T>(exactly source: T) where T : BinaryInteger {
+        self.init(Double(source))
+    }
+    
+    typealias IntegerLiteralType = Int
+    
+ static func - (lhs: Value, rhs: Value) -> Value {
         return lhs + (-1 * rhs)
     }
     
@@ -65,6 +94,8 @@ class Value: CustomStringConvertible, Equatable, Hashable {
     static func - (lhs: Double, rhs: Value) -> Value {
         return Value(lhs) - rhs
     }
+    
+    
     
     static func / (lhs: Value, rhs: Value) -> Value {
         return lhs * (rhs ** (-1))
@@ -77,6 +108,7 @@ class Value: CustomStringConvertible, Equatable, Hashable {
     static func / (lhs: Double, rhs: Value) -> Value {
         return lhs * (rhs ** (-1))
     }
+    
     
     static func * (lhs: Value, rhs: Value) -> Value {
         let out = Value(lhs.data * rhs.data, children: [lhs, rhs], operations: "*")
@@ -95,6 +127,14 @@ class Value: CustomStringConvertible, Equatable, Hashable {
     
     static func * (lhs: Double, rhs: Value) -> Value {
         return Value(lhs) * rhs
+    }
+    
+    static func *= (lhs: inout Value, rhs: Value) {
+        lhs = lhs * rhs
+    }
+    
+    static func *= (lhs: inout Value, rhs: Double) {
+        lhs = lhs * rhs
     }
     
     
@@ -129,6 +169,7 @@ class Value: CustomStringConvertible, Equatable, Hashable {
                 sorted.append(vertex)
             }
         }
+        
         buildTopologicalSort(self)
         
         self.grad = 1.0
@@ -137,140 +178,7 @@ class Value: CustomStringConvertible, Equatable, Hashable {
             value._backward()
         }
     }
-    
-    func hash(into hasher: inout Hasher) {
-            hasher.combine(self.data)
-            hasher.combine(self.grad)
-            hasher.combine(self.operations)
-        }
-
 }
 
 
 
-class Neuron : CustomStringConvertible {
-    var w: [Value]
-    var b: Value
-    
-    init(_ nin: Int) {
-        self.w = Array(repeating: Value(Double.random(in: -1...1)), count: nin)
-        self.b = Value(Double.random(in: -1...1))
-    }
-    
-    var parameters: [Value] {
-        return w + [b]
-    }
-    
-    var description: String {
-        var description = "( "
-        for n in 0..<w.count {
-            description += "(\(n+1))" + w[n].description
-        }
-        description += "(b)" + b.description
-        return description + " )"
-    }
-    
-    func callAsFunction(_ x: [Double]) -> Value {
-        var activation: Value = zip(w,x).map { $0 * $1 }.reduce(self.b, +)
-        activation = activation.tanh()
-        return activation
-    }
-    
-    func callAsFunction(_ x: [Value]) -> Value {
-        var activation: Value = zip(w,x).map { $0 * $1 }.reduce(self.b, +)
-        activation = activation.tanh()
-        return activation
-    }
-}
-
-
-class Layer : CustomStringConvertible {
-    
-    var neurons: [Neuron]
-    
-    var parameters: [Value] {
-        neurons.map { $0.parameters }.reduce([], +)
-    }
-    
-    var description: String {
-        var description = "\n"
-        for neuron in neurons {
-            description += neuron.description + "\n"
-        }
-        return description
-    }
-    
-    
-    init(_ nin: Int, _ nout: Int) {
-        self.neurons = Array(repeating: Neuron(nin), count: nout)
-    }
-    
-    func callAsFunction(_ x: [Double]) -> [Value] {
-        neurons.map { $0(x) }
-    }
-    
-    func callAsFunction(_ x: [Value]) -> [Value] {
-        neurons.map { $0(x) }
-    }
-    
-    
-}
-
-class MLP : CustomStringConvertible {
-    
-    var layers: [Layer]
-    
-    var parameters: [Value] {
-        return layers.map { $0.parameters }.reduce([], +)
-    }
-    
-    var description: String {
-        var description = ""
-        for layer in n.layers {
-            description += layer.description + "\n\n\n"
-        }
-        return description
-    }
-    
-    init(_ nin: Int, _ nouts: [Int] ) {
-        let totalnRequired = [nin] + nouts
-        self.layers = (0..<nouts.count).map { Layer(totalnRequired[$0], totalnRequired[$0 + 1]) }
-    }
-    
-    func callAsFunction(_ x: [Double]) -> [Value] {
-        var out = layers.first!(x)
-        for layer in layers[1...] {
-            out = layer(out)
-        }
-        return out
-    }
-    
-}
-
-var n = MLP(3, [4, 4, 1])
-
-var xs = [
-    [2.0, 3.0, -1.0],
-    [3.0, -1.0, 0.5],
-    [0.5, 1.0, 1.0],
-    [1.0, 1.0, -1.0]
-]
-
-var ys = [1.0, -1.0, -1.0, 1.0]
-var ypred : [Value] = xs.map { value in
-    return n(value).first!
-}
-
-var loss : Value
-var stepSize : Double
-for _ in 0...1 {
-    ypred = xs.map { value in
-        return n(value).first!
-    }
-    loss = zip(ys,ypred).map { ( $0 - $1 ) ** 2 }.reduce(Value(0), +)
-    n.parameters.forEach { $0.grad = 0 }
-    loss.backward()
-    stepSize = 0.001
-    n.parameters.forEach { $0.data -= stepSize * $0.grad }
-}
-print(n)
--- micrograd/nn.swift
+++ micrograd/nn.swift
@@ -6,3 +6,101 @@
 //
 
 import Foundation
+class Neuron : CustomStringConvertible {
+    var w: [Value]
+    var b: Value
+    
+    init(_ nin: Int) {
+        self.w = (0..<nin).map{ _ in Value(Double.random(in: -1...1)) }
+        self.b = Value(Double.random(in: -1...1))
+    }
+    
+    var parameters: [Value] {
+        return w + [b]
+    }
+    
+    var description: String {
+        var description = "( "
+        for n in 0..<w.count {
+            description += "(\(n+1))" + w[n].description
+        }
+        description += "(b)" + b.description
+        return description + " )"
+    }
+    
+    func callAsFunction(_ x: [Double]) -> Value {
+        var activation: Value = zip(w,x).map { $0 * $1 }.reduce(self.b, +)
+        activation = activation.tanh()
+        return activation
+    }
+    
+    func callAsFunction(_ x: [Value]) -> Value {
+        var activation: Value = zip(w,x).map { $0 * $1 }.reduce(self.b, +)
+        activation = activation.tanh()
+        return activation
+    }
+}
+
+
+class Layer : CustomStringConvertible {
+    
+    var neurons: [Neuron]
+    
+    var parameters: [Value] {
+        neurons.map { $0.parameters }.reduce([], +)
+    }
+    
+    var description: String {
+        var description = "\n"
+        for neuron in neurons {
+            description += neuron.description + "\n"
+        }
+        return description
+    }
+    
+    
+    init(_ nin: Int, _ nout: Int) {
+        self.neurons = (0..<nout).map{ _ in Neuron(nin) }
+    }
+    
+    func callAsFunction(_ x: [Double]) -> [Value] {
+        neurons.map { $0(x) }
+    }
+    
+    func callAsFunction(_ x: [Value]) -> [Value] {
+        neurons.map { $0(x) }
+    }
+    
+    
+}
+
+class MLP : CustomStringConvertible {
+    
+    var layers: [Layer]
+    
+    var parameters: [Value] {
+        return layers.map { $0.parameters }.reduce([], +)
+    }
+    
+    var description: String {
+        var description = ""
+        for layer in self.layers {
+            description += layer.description + "\n\n\n"
+        }
+        return description
+    }
+    
+    init(_ nin: Int, _ nouts: [Int] ) {
+        let totalnRequired = [nin] + nouts
+        self.layers = (0..<nouts.count).map { Layer(totalnRequired[$0], totalnRequired[$0 + 1]) }
+    }
+    
+    func callAsFunction(_ x: [Double]) -> [Value] {
+        var out = layers.first!(x)
+        for layer in layers[1...] {
+            out = layer(out)
+        }
+        return out
+    }
+    
+}
